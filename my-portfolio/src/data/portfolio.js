0258f98c789e0ce1f512c1958ae2abba4e034f9a
Ahtisham992 <shamimuhammad77@gmail.com>
2026-03-27 00:13:08 +0500

    updated readme

--- my-portfolio/src/data/portfolio.js
+++ my-portfolio/src/data/portfolio.js
@@ -126,7 +126,7 @@ export const projects = [
     media: {
       thumbnail: "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?w=800&q=80",
       images: [
-        "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?w=1200&q=80",
+        "/projectSS/salescare/1.png",
         "https://images.unsplash.com/photo-1460925895917-afdab827c52f?w=1200&q=80",
         "https://images.unsplash.com/photo-1551288049-bebda4e38f71?w=1200&q=80"
       ],
